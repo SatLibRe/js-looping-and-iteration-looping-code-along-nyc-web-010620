72b5eefd4ae4a18395ff5d1955365b2fdb07ac70
Miles Higbie <miles.higbie@gmail.com>
2020-02-17 00:29:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function writeCards(arr){
   const t_arr = []
   for(let i = 0; i < arr.length; i++){
-    t_arr.push(`Thank you ${arr[i]}`)
+    t_arr.push(`Thank you, ${arr[i]}, `)
   }
   return t_arr
 }
